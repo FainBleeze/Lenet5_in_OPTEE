504a45589fcb735ec5f185f58526c8f700a98166
FainBleeze <frdm@foxmail.com>
2020-12-14 15:46:53 +0800

    Add Makefiles

---
+++ TEE_source_code/ta/Android.mk
@@ -0,0 +1,4 @@
+LOCAL_PATH := $(call my-dir)
+
+local_module := b13bc418-3b81-11eb-adc1-0242ac120002.ta
+include $(BUILD_OPTEE_MK)
---
+++ TEE_source_code/ta/sub.mk
@@ -0,0 +1,6 @@
+global-incdirs-y += include
+srcs-y += lenet.c
+srcs-y += lenet5_ta.c
+
+# To remove a certain compiler flag, add a line like this
+#cflags-template_ta.c-y += -Wno-strict-prototypes
