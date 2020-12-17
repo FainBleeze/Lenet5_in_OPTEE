4924def9482a38d8ac8cf5ad811b05d29c340fa7
FainBleeze <frdm@foxmail.com>
2020-12-17 13:40:10 +0800

    Update Makefile

--- TEE_source_code/ta/sub.mk
+++ TEE_source_code/ta/sub.mk
@@ -1,6 +1,7 @@
 global-incdirs-y += include
 srcs-y += lenet5_ta.c
-cflags-lenet.c-y += -lm
 srcs-y += lenet.c
+srcs-y += e_exp.c
+srcs-y += e_sqrt.c
 # To remove a certain compiler flag, add a line like this
 #cflags-template_ta.c-y += -Wno-strict-prototypes
