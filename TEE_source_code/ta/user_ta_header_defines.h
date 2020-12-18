afa611412e19979e29423cb40e1dd588feef85c9
FainBleeze <frdm@foxmail.com>
2020-12-18 15:20:54 +0800

    Resize heap and stack in TA

--- TEE_source_code/ta/user_ta_header_defines.h
+++ TEE_source_code/ta/user_ta_header_defines.h
@@ -24,10 +24,10 @@
 #define TA_FLAGS			TA_FLAG_EXEC_DDR
 
 /* Provisioned stack size */
-#define TA_STACK_SIZE			(2 * 1024)
+#define TA_STACK_SIZE			(1024 * 1024)
 
 /* Provisioned heap size for TEE_Malloc() and friends */
-#define TA_DATA_SIZE			(32 * 1024)
+#define TA_DATA_SIZE			(512 * 1024)
 
 /* The gpd.ta.version property */
 #define TA_VERSION	"1.0"
